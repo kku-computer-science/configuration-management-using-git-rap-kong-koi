0d1f4fdf045153f4dc468941776f811fcb2f0426
o0Hadez0o <tawaan.ut@kkumail.com>
2025-12-10 00:27:22 +0700

    implemented bubblesort

--- Project/src/algorithms/BubbleSort.java
+++ Project/src/algorithms/BubbleSort.java
@@ -1,5 +1,42 @@
 package algorithms;
 
 public class BubbleSort {
+	
+	public static void sort(int array[]) {
+		
+		int n = array.length;
+		int i,j,temp;
+		boolean swapped;
+		
+		for(i=0; i < n-1; i++) {
+			swapped = false;
+			
+			for(j=0; j < n-i-1; j++) {
+				if (array[j] > array[j+1]) {
+		
+					temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    
+                    swapped = true;
+                    
+				}
+			}
+			
+			if(swapped == false) {
+				break;
+			}
+			
+		}
+		
+	}
+	
+	public static void printArray(int arr[]){
+        int i;
+        for (i=0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        
+        System.out.println();
+    }
 
 }
