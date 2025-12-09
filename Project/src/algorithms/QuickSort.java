4d56a358d22b3004590237385f145ad522f9019f
adithep2422 <adithep.ma@kkumail.com>
2025-12-09 23:30:15 +0700

    update QuickSort.java

--- Project/src/algorithms/QuickSort.java
+++ Project/src/algorithms/QuickSort.java
@@ -1,37 +1,42 @@
 package algorithms;
 
 public class QuickSort {
-	
-    static int partition(int[] arr, int low, int high) {
-        
-        int pivot = arr[high];
-        int i = low - 1;
 
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        
-        swap(arr, i + 1, high);  
-        return i + 1;
-    }
+	static int partition(int[] arr, int low, int high) {
 
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+		int pivot = arr[high];
+		int i = low - 1;
 
-    // quick sort function
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            
-            int pi = partition(arr, low, high);
+		for (int j = low; j <= high - 1; j++) {
+			if (arr[j] < pivot) {
+				i++;
+				swap(arr, i, j);
+			}
+		}
 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
+		swap(arr, i + 1, high);
+		return i + 1;
+	}
+
+	static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	// quick sort function
+	static void quickSort(int[] arr, int low, int high) {
+		if (low < high) {
+
+			int pi = partition(arr, low, high);
+
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+
+			for (int val : arr) {
+				System.out.println("Sorted Array : " + val);
+			}
+		}
+	}
+
 }
